0ff090c7d8456980b4c3c921fd33cbacdb091a3b
konentung <84655730+konentung@users.noreply.github.com>
2024-03-14 11:11:10 +0800

    feature-0314 1.鍵盤功能(ESC關閉畫面) 2.動畫作業部分功能

--- init.py
+++ init.py
@@ -6,12 +6,42 @@ def main():
     pygame.display.set_caption("init")
     width, height = 400,300
     screen = pygame.display.set_mode((width, height))
-    while True:        
+    clock = pygame.time.Clock()
+    
+    # 彙整圖片
+    img_bg = pygame.image.load("bg.png")
+    img_chara = [
+        pygame.image.load("chara1.png"),
+        pygame.image.load("chara2.png"),
+    ]
+    tmr = 0
+    
+    running = True
+    while running:
+        tmr = tmr + 1
+        
         for event in pygame.event.get(): # 迴圈處理pygame事件
             if event.type == pygame.QUIT: # 當視窗的x被按下
                 pygame.quit() # 解除初始化pygame模組
                 sys.exit() # 結束程式
-
+            # 鍵盤的事件
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                if event.key == pygame.K_F1:
+                    screen = pygame.display.set_mode((640, 360))
+                if event.key == pygame.K_F11:
+                    screen = pygame.display.set_mode((640, 360), pygame.FULLSCREEN)
+                    
+        x = tmr%160
+        for i in range(5):
+            screen.blit(img_bg, [i*160-x, 0])
+            
+        # 更新遊戲畫面       
+        screen.fill((0,0,0))
+        screen.blit(img_chara[tmr%2], (224,160))
+        pygame.display.update()
+        clock.tick(5)
 
 if __name__ == '__main__':
     main()
